14b6113bb4e0aeffadcc8a5235875f02cb1bcf01
ifahimDigiden <iftekhar.fahim@digidenit.com>
2025-05-12 12:50:51 +0600

    Sidebar Menu selector changed by modifying sidebarGenerator function

--- src/components/shared/DashboardSidebar.tsx
+++ src/components/shared/DashboardSidebar.tsx
@@ -1,4 +1,3 @@
-import { isValidElement, ReactElement } from "react";
 import { Layout, Menu, MenuProps } from "antd";
 import { useCurrentToken } from "../../redux/feature/auth/authSlice";
 import { useAppSelector } from "../../redux/hooks";
@@ -14,6 +13,7 @@ const { Sider } = Layout;
 
 const DashboardSidebar = () => {
   const token = useAppSelector(useCurrentToken);
+  const location = useLocation();
 
   let user;
 
@@ -41,46 +41,6 @@ const DashboardSidebar = () => {
       break;
   }
 
-  const findActiveMenuKey = (
-    items: MenuProps["items"] | undefined,
-    pathname: string
-  ): string | undefined => {
-    for (const item of items || []) {
-      if (!item) continue;
-
-      if ("key" in item && "label" in item) {
-        const { label } = item;
-        if (isValidElement(label)) {
-          const element = label as ReactElement<{ to: string }>;
-          if (element.props.to === pathname) {
-            return item.key as string;
-          }
-        }
-      }
-
-      if ("children" in item && Array.isArray(item.children)) {
-        for (const child of item.children) {
-          if (!child) continue;
-
-          if ("label" in child && "key" in child) {
-            const { label } = child;
-            if (isValidElement(label)) {
-              const element = label as ReactElement<{ to: string }>;
-              if (element.props.to === pathname) {
-                return child.key as string;
-              }
-            }
-          }
-        }
-      }
-    }
-
-    return undefined;
-  };
-
-  const location = useLocation();
-  const selectedKey = findActiveMenuKey(sidebarItems, location.pathname);
-
   return (
     <Sider
       breakpoint="lg"
@@ -93,10 +53,9 @@ const DashboardSidebar = () => {
       <div className="h-[4rem] flex justify-center items-center text-gray-50 mb-5 bg-white">
         <img src={logo} className="w-40" />
       </div>
-
       <Menu
         mode="inline"
-        selectedKeys={selectedKey ? [selectedKey] : []}
+        selectedKeys={[location.pathname]} // sync with route
         items={sidebarItems}
         className="bg-[#60032a] "
         style={{
--- src/utils/sidebarItemsGenerator.tsx
+++ src/utils/sidebarItemsGenerator.tsx
@@ -1,34 +1,34 @@
-import { Link } from "react-router-dom";
-import { TSidebarItem, TUserPath } from "../types/sidebar.type";
+import { Link } from 'react-router-dom';
+import { TSidebarItem, TUserPath } from '../types/sidebar.type';
 
 export const sidebarItemsGenerator = (items: TUserPath[], role: string) => {
   const sidebarItems = items.reduce((acc: TSidebarItem[], item) => {
     if (item.show === false) return acc;
+
+    const fullPath = '/' + role + item?.path;
+
     if (item.path && item.name) {
       acc.push({
-        key: item.name,
+        key: fullPath, // use full path as key
         icon: item?.icon,
-        label: <Link to={`/${role}${item.path}`}>{item.name}</Link>,
+        label: <Link to={fullPath}>{item?.name}</Link>,
       });
     }
 
     if (item.children && item.name) {
-      const children = item.children
-        .filter((child) => child.show !== false)
-        .map((child) => ({
-          key: child.name!,
-          icon: child?.icon,
-          label: <Link to={`/${role}${child?.path}`}>{child?.name}</Link>,
-        }));
-
-      if (children.length > 0) {
-        acc.push({
-          key: item.name,
-          label: item.name,
-          icon: item.icon,
-          children,
-        });
-      }
+      acc.push({
+        key: item.name, // for submenu grouping
+        label: item.name,
+        icon: item.icon,
+        children: item.children.map((child) => {
+          const childPath = '/' + role + child?.path;
+          return {
+            key: childPath, // use full child path as key
+            icon: child?.icon,
+            label: <Link to={childPath}>{child?.name}</Link>,
+          };
+        }),
+      });
     }
 
     return acc;
@@ -36,5 +36,3 @@ export const sidebarItemsGenerator = (items: TUserPath[], role: string) => {
 
   return sidebarItems;
 };
-
-
